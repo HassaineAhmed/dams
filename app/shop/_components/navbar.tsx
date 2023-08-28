8c84de8def2b56cdfdb2dc04218818396a4ccd7b
fablorv <fablorvdz@gmail.com>
2023-08-28 19:23:15 +0000

    fixing conflict

--- app/shop/_components/navbar.tsx
+++ app/shop/_components/navbar.tsx
@@ -9,15 +9,6 @@ import { useState } from "react"
 
 export function Navbar() {
 
-<<<<<<< HEAD
-  return <div className="bg-black animate-fadeInFromUp lg:w-[1100px] lg:h-[126px] lg:px-[14px] relative px-4 border-sd h-[70px] font-lora flex justify-between items-center w-[100%]">
-    <div className='flex items-center gap-1 lg:gap-2'>
-      <div className="w-[30px] h-[30px] lg:w-[60px] lg:h-[60px] justify-center items-center flex">
-      <Image src='/logo.svg' height={90} width={90} alt='logo' />
-      </div>
-	
-      <p className='text-xl lg:text-[43px] text-gold font-semibold'>DAMS</p>
-=======
   return (
     <div
       className="animate-fadeInFromUp lg:w-[1800px] lg:h-[126px] lg:px-[14px] relative px-4 border-sd h-[70px] font-lora flex justify-between items-center w-[100%]">
@@ -31,7 +22,6 @@ export function Navbar() {
         <Menu />
       </div>
 
->>>>>>> c00f948ff02e0c5ecad9ff9fdcf32edf3ca255b2
     </div>
   )
 }
