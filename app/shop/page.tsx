72fef51684c59221b4f668c351204e29a17f024d
HassaineAhmed <ahmed.hassaine432@gmail.com>
2023-09-19 17:41:28 +0100

    switched to axios

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -5,14 +5,14 @@ import { TabSection } from "./_components/tabSections"
 import { FAQs } from "./_components/faqs"
 import { ContactFooter, DownFooter } from "./_components/footer"
 import { revalidateTag } from "next/cache"
+import axios from "~/axios"
 
 export default async function Shop() {
   const domainName = process.env.DOMAIN_NAME
   //revalidateTag("mainData")
   //  await fetch(`${domainName}/api/revalidate-data` , { method : "GET", cache : "no-cache"}).then( res => console.log("data revalidated"));
-  revalidateTag("mainData")
-  const res = await fetch(`${domainName}/api/pages/home`, { cache: "no-cache", next: { revalidate: 10, tags: ["mainData"] }, method: "GET" })
-  const { categories, tabs_products, faqs } = await res.json()
+  const res = await axios.get("/api/pages/home")
+  const { categories, tabs_products, faqs } = res.data
 
   return (<div className="pt-1 lg:pt-2 flex bg-pr lg:gap-2 flex-col gap-1">
     <HeroSection />
